46a3b121f8decd0c1b9815c2ae8256d889389ab6
sebastianbaquerop <34309142+sebastianbaquerop@users.noreply.github.com>
2024-11-30 13:00:11 -0500

    [commit][feature/fullstack] Feat: fullstack service

--- pages/fullstack-solutions/fullstack-solutions.js
+++ pages/fullstack-solutions/fullstack-solutions.js
@@ -1,4 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Fullstack Solutios Service content loaded");
-    // Lógica específica de los servicios
-});
+// script.js
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre').value;
+    const email = document.getElementById('email').value;
+    const mensaje = document.getElementById('mensaje').value;
+  
+    if (nombre && email && mensaje) {
+      alert("Gracias, ${nombre}. Hemos recibido tu mensaje: ${mensaje}. Te contactaremos pronto a ${email}.");
+      document.getElementById('contactForm').reset();
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
+  });
+  
+  document.getElementById('learnMoreBtn').addEventListener('click', function () {
+    const extraInfo = document.getElementById('extraInfo');
+    if (extraInfo.style.display === 'none') {
+      extraInfo.style.display = 'block';
+    } else {
+      extraInfo.style.display = 'none';
+    }
+  });
